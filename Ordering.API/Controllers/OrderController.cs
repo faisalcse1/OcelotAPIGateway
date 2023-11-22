9ae5614e6ed96d8eda9c6cd866f907fe465b010c
Md Faisal <faisalcse1@gmail.com>
2023-11-22 15:25:46 +0600

    Add project files.

---
+++ Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Order has been loaded.");
+        }
+        [HttpGet]
+        public IActionResult Get(int id) 
+        {
+            return Ok(id);
+        }
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            return Ok("Order has been created "+name);
+        }
+    }
+}
